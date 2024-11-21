f37a1958d5e1098236db4baae62188df1970f0b1
Navi025 <vinayaknavnitya20@gmail.com>
2024-11-21 11:55:20 +0530

    new file:   Java Basics/JFunction.java 	new file:   Java Basics/JavaBasics.java 	new file:   Java Basics/JavaLoops.java

---
+++ Java Basics/JFunction.java
@@ -0,0 +1,255 @@
+  
+            /* JAVA FUNCTIONS 
+
+import java.util.*;
+public class JFunction{
+    
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("I'm NAVLOK Device 07");
+    }
+
+    public static int calculatesum(int num1, int num2){
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = calculatesum(a,b); //here the varible sum is diffent from the above one coz its stored in another function.
+        printHelloWorld();
+    }
+}*/
+
+
+/* CALL BY VALUE */
+
+/* PRODUCT OF TWO NOS 
+
+public class JFunction{
+    public static int multiply(int a,int b){
+        int product = a*b;
+        return product;
+    }
+
+    public static void main(String args[]){
+        int x = 5;
+        int y = 4;
+        int product = multiply(x,y);
+        System.out.print("The product = " + product);
+    }
+} */   
+
+/* FACTORIAL OF N 
+import java.util.*;
+public class JFunction{
+    
+    public static int factorial(int n){
+        int fact = 1;
+        for (int i=1; i<=n; i++){
+            fact = fact*i;
+        }
+        return fact;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Wanna know the factorial? \n Enter your no here : ");
+        int n = sc.nextInt();
+        int fact = factorial(n);
+        System.out.println("The factorial is " + fact);
+    }
+
+}
+
+import java.util.*;
+public class JFunction{
+    
+    public static int factorial(int n){
+        int fact = 1;
+        for (int i=1; i<=n; i++){
+            fact = fact * i;
+        } 
+        return fact;
+    }
+    public static int bincoff(int n, int r){
+        int nfact = factorial(n);
+        int rfact = factorial(r);
+        int n_rfact = factorial(n-r);
+        int bincoff = nfact / (rfact * n_rfact);
+        return bincoff;
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Wanna Calculate Binomial Co-efficient? \nProvide following values: \nThe value of n = ");
+        int n = sc.nextInt();
+        System.out.print("The value of r = ");
+        int r = sc.nextInt();
+        int bincoff = bincoff(n,r);
+        System.out.print("The Binomial Co-efficient is : " + bincoff);    
+        }
+}*/
+
+            /* FUNCTIONAL OVERLOADING 
+
+                Diff No of Parameters
+
+public class JFunction{
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static int sum( int a , int b , int c){
+        return a+b+c;
+    }
+
+    public static void main(String args[]){
+       int s1 = sum(3,5);
+       int s2 = sum(3,5,7);
+       System.out.println(s1+  " " + s2 );
+       System.out.print(s2);
+
+    }
+}       
+            Different type
+
+public class JFunction{
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static float sum(float a, float b){
+        return a+b;
+    }
+
+    public static void main(String args[]){
+        System.out.println(sum(4,8));
+        System.out.print(sum(4.2f,8.6f));
+    }
+}*/
+
+        /* IS PRIME NORMAL & OPTIMISED CASE */
+
+/* NORMAL 
+
+public class JFunction{
+    public static boolean isPrime(int n){
+        if (n==2){
+            return true;
+        }
+
+        for (int i=2; i<=n-1; i++){
+            if (n%i==0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        System.out.print(isPrime(1));
+    }   
+
+}*/
+
+/* OPTIMISED 
+
+//import java.math.*;
+public class JFunction{
+    public static boolean isPrime(int n){
+        if (n==2){
+            return true;
+        }
+
+        for (int i=2; i<=(Math.sqrt(n)); i++){
+            if (n%i==0){
+                return false;
+            }
+       
+        }
+        return true;
+    }
+
+    public static void main(String args[] ){
+        System.out.print(isPrime(7));
+    }
+}
+
+
+//import java.Scanner;
+public class JFunction{
+    
+    public static boolean isPrime(int n){
+        if (n==2){
+            return true;
+        }
+        for (int i=2; i<=Math.sqrt(n); i++){
+            if (n % i == 0){
+                return false;
+            }    
+        }
+        return true;
+    }
+    
+    public static int primeInRange(int n){
+        for (int i=2; i<=n; i++){
+            if (isPrime(i)==true){
+                System.out.print(i + " ");
+            }
+        } 
+        return n;
+    }
+
+    public static void main(String args[]){
+        primeInRange(100);
+    }
+}
+        /* Binary to Decimal
+
+public class JFunction{
+    public  static int binaryToDecimal(int  binNum){
+        int myNum = binNum;
+        int pow = 0;
+        int dec = 0;
+
+        while (binNum>0){
+            int lastDigit = binNum % 10;
+            dec += lastDigit * (int)Math.pow(2,pow); 
+            pow++;
+            binNum /= 10; 
+        }
+        
+        System.out.println("Decimal value of " + myNum + " = " + dec );
+        return dec;
+    }
+    public static void main(String args[]){
+        binaryToDecimal(1111);
+    }
+}*/
+
+    /* Decimal To Binary */
+
+public class JFunction{
+    public static int decimalToBinary(int n){
+        int pow = 0;
+        int myNum = n;
+        int binNum = 0;
+
+        while (n>0){
+            int rem = n % 2;
+            binNum += rem * Math.pow(10,pow);
+            pow++;
+            n/=2;
+        }
+        System.out.print("The Binary for " + myNum + " in " + binNum);
+        return binNum;    
+    }
+
+    public static void main(String args[]){
+        decimalToBinary(5);
+    }
+}
---
+++ Java Basics/JavaLoops.java
@@ -0,0 +1,386 @@
+ 
+ /* LOOPS 
+
+
+ public class JavaLoops{
+    public static void main(String args[]){
+        int counter = 0;
+        while (counter < 3){
+            System.out.println("Navnitya is a Software Developer");
+            counter++;
+        }
+
+    }
+}*/
+
+/* Print no 1 to 10 
+
+public class JavaLoops{
+    public static void main(String args[]){
+        int counter = 1;
+        while (counter <=10000){
+            System.out.print(counter+" ");
+            counter++;
+        }  
+    }
+}
+
+import java.util.*;
+public class JavaLoops{
+    public static void main(String args[]){
+        System.out.print("Enter tha range of nos : ");
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+        int counter = 1;
+        while (counter <= range){
+            System.out.print(counter + " ");
+            counter++;
+        }
+    }
+}*/
+
+/* SUM OF FIRST N NATUTAL NOS. 
+
+import java.util.*;
+public class JavaLoops{
+    public static void main(String args[]){
+        
+        System.out.print("Enter teh range of natural nos to be added: ");
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+        int i = 1;
+
+        while (i <= n ){
+            sum = sum +i;
+            i++;
+            
+        }    
+        System.out.print(sum);
+
+    }
+}*/
+
+/* FOR LOOP 
+
+public class JavaLoops{
+    public static void main(String args[]){
+        for (int i = 1; i <= 4; i++ ){
+            System.out.println("Im Navnitya Vinayak");
+        }
+    }
+}
+
+public class JavaLoops{
+    public static void main(String args[]){
+        for (int line = 1; line<=4; line++){;
+        System.out.println("****");
+        }
+
+    }
+}
+
+//10899 10000+800+90+9
+//V-I-P-O
+import java.util.*;
+public class JavaLoops{
+    public static void main(String args[]){
+    System.out.print("Enter a number : ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+     while (n>0){
+        int r = n%10;
+        System.out.print(r+"");
+        n=n/10;
+     
+        } 
+    }    
+}
+
+import java.util.*;
+public class JavaLoops{
+    public static void main(String args[]){
+    System.out.print("Enter a number : ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int rev = 0;
+     while (n > 0){
+        int r = n % 10;
+        rev = rev * 10 + r;
+        n = n / 10;
+     
+        }
+    System.out.print(rev); 
+    }    
+}*/
+
+/* break */
+
+/* Keep entering a number till user enters number multiple of 
+
+import java.util.*;
+public class JavaLoops{
+    public static void main(String args[]){
+       
+        System.out.print("Enter any no. other tha nmultiple of 10 : ");
+       
+        Scanner sc = new Scanner(System.in);
+        //int num = sc.nextInt();
+       
+        while(true){
+        System.out.print("Enter any no. other tha nmultiple of 10 : ");
+        int num = sc.nextInt();
+        System.out.print(num);
+            if (num % 10 == 0){
+                break;
+            }
+        
+        }
+        
+    }
+}*/
+
+/* CONTINUE STATEMENT 
+
+public class JavaLoops{
+    public static void main(String args[]){
+        for (int i = 1; i <= 5;i++){ 
+            if (i==3){
+                continue;
+            }
+        System.out.print(i + " ");
+        }
+    }
+}*/
+
+/* Keep entering numbers till user enters number multiple of 
+
+import java.util.*;
+public class JavaLoops{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println("ENter a num: ");
+            int num = sc.nextInt();
+            if (num % 10==0){
+                continue;
+            }
+        System.out.println(num);
+        }
+    }
+}*/
+
+
+/* CHECK IF A NO IS PRIME 
+import java.util.*;
+public class JavaLoops{
+    public static void main(String args[]){
+        
+        System.out.print("Enter a num : ");
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        for (int i=2 ; i<=n-1 ; i++){
+            if (n % i == 0){ // n is a multiple of i (i is not equal to 1 or n)
+                isPrime = false;
+            }
+        }
+        if (isPrime == true){
+            System.out.println("It is Prime no.");
+
+        }
+        else{
+            System.out.println("The no is not prime");
+        }
+    }
+}
+
+public class JavaLoops{
+    public static void main(String[]args){
+        for ( int i = 0 ; i<5 ; i++) {
+        System.out.println("Hello");
+        i+=2;
+        }
+    }
+}*/
+
+/*Question2:
+Write a program that reads a set of itegers, and then prints the sum of the
+even and odd integers. 
+
+import java.util.*;
+public class JavaLoops{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int choice;
+        int evenSum = 0;
+        int oddSum = 0;
+        
+        do{ 
+            System.out.print("Enter a no.: ");
+            num = sc.nextInt();
+
+            if (num % 2 == 0){
+                evenSum += num;
+            }
+            else {
+                oddSum += num;
+
+            }
+            System.out.println("Do you want to continue? press 1 if YES or press 0 if NO.");
+            choice = sc.nextInt();
+        }
+        while (choice == 1);
+            System.out.println("Even Sum : " + evenSum);
+            System.out.println("Odd Sum : " + oddSum);
+        
+    }
+}
+
+FACTORIAL OF A NUMBER
+
+import java.util.*;
+public class JavaLoops{ 
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:  ");
+        int n = sc.nextInt();
+        int factN = 1;
+        for (int i=1 ; i<=n ; i++){
+        factN = factN*(i);
+        }
+        System.out.println("Factorial of the no is : " + factN);
+
+    }
+}
+import java.util.Scanner;
+public class JavaLoops{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a natural no: ");
+        int n = sc.nextInt();
+        int table = n;
+        System.out.println("The required table is: ");
+        for(int i=1 ; i<=10 ; i++){
+            table = n*i;
+            System.out.println(n + " * " + i + " = " + table );
+        }
+        
+    }
+}*/
+
+/* NESTED LOOPS PATTURN I 
+
+        *
+        **
+        ***
+        ****
+
+public class JavaLoops{
+    public static void main(String args[]){
+        for (int line=1; line<=10; line++){
+            for (int star=1; star<=line; star++){
+                System.out.print("*");
+            }
+            System.out.println(); //next line
+        }
+    }
+    
+}
+
+        ****
+        ***
+        **
+        *
+        
+public class JavaLoops{
+    public static void main(String args[]){
+      for(int line=1; line<=10; line ++){
+        for (int star=10; star>=line; star--){
+            System.out.print("*");
+
+        }
+        System.out.println();
+      }  
+    }
+}*/
+/* ANOTHER MEATHOD 
+
+public class JavaLoops{
+    public static void main(String args[]){
+        int n = 22;
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+        A
+        BC
+        DEF
+        GHIJ
+
+public class JavaLoops{
+    public static void main(String args[]){
+        int n = 4;
+        char ch = 'A';
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i ; j++){
+                System.out.print(ch);
+                ch++;
+            }
+        System.out.println();
+        }
+    } 
+}       
+
+        1
+        12
+        123
+        1234
+
+public class JavaLoops{
+    public static void main(String args[]){
+        int n = 10;
+       // int num = 1;
+        for (int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+               // for(num=1; num<=i; num++){
+
+                
+                //num++;
+            }
+        System.out.println();
+        }
+    }
+}
+
+        ****
+        *  *
+        *  *
+        *****/
+public class JavaLoops{
+    public static void main(String args[]){
+        for(int line=1; line<=4; line++){
+            if(line==2 && line ==3){
+                    System.out.print("*   *");
+                }
+                else{
+            for(int star=1; star<=5; star++){
+                
+                System.out.print("*");
+                } 
+            }
+            System.out.println();
+        }
+    }
+}
