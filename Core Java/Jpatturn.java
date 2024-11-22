542c9266be50d46b110322ac3b2d791b74f3c830
Navi025 <vinayaknavnitya20@gmail.com>
2024-11-22 23:40:57 +0530

    modified:   Core Java/Jpatturn.java 	new file:   DSA/Arrays.class 	modified:   DSA/Arrays.java

--- Core Java/Jpatturn.java
+++ Core Java/Jpatturn.java
@@ -198,7 +198,6 @@ public class Jpatturn{
                     *
 */
     public static void diamond(int n){
-        int count = 1;
         for (int i=1; i<=n; i++){
             for (int j=1; j<=n-i; j++){
                 System.out.print("   ");
